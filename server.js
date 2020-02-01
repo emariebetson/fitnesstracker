f0744d87097795e03f5790b3671e6a1c00b6ba05
Liz <52430633+emariebetson@users.noreply.github.com>
2020-02-02 04:02:06 +0700

    updating port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const seeders = require('./seeders/seed');
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
