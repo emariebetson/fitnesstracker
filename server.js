6f86090ab5ee5cfd99d76f145fe72fa7f7f28bc0
Liz <52430633+emariebetson@users.noreply.github.com>
2020-02-02 04:05:22 +0700

    updating routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const APIroutes = require('./routes/api');
+const HTMLroutes = require('./routes/html');
 const seeders = require('./seeders/seed');
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -12,8 +14,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true
 });
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.use(APIroutes);
+app.use(HTMLroutes);
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
