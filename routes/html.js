69f35e4040754dd8d0021ab30568d3f09bab37c1
Liz <52430633+emariebetson@users.noreply.github.com>
2020-02-02 05:39:14 +0700

    updating routes

--- routes/html.js
+++ routes/html.js
@@ -1,12 +1,18 @@
-const router = require('express').Router();
+//routes for stats and exersize
+const express = require('express');
+const router = express.Router();
 const path = require('path');
-/* route for exercise.html */
-router.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './../public/index.html'));
 });
-/* route for stats.html */
+
 router.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/stats.html'));
+  res.sendFile(path.join(__dirname, './../public/stats.html'));
+});
+
+router.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, './../public/exercise.html'));
 });
 
 module.exports = router;
